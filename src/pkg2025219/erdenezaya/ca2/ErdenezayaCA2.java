97507a59fa6595e8ec274745653abd2953ac6188
Erdenezaya <2025219@student.cct.ie>
2025-11-19 18:23:09 +0000

    creating main menu
    5 options menu

--- src/pkg2025219/erdenezaya/ca2/ErdenezayaCA2.java
+++ src/pkg2025219/erdenezaya/ca2/ErdenezayaCA2.java
@@ -4,6 +4,8 @@
  */
 package pkg2025219.erdenezaya.ca2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Erdenezaya
@@ -14,7 +16,50 @@ public class ErdenezayaCA2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        // Menu loop
+        while (true) {
+            System.out.println("\n--- Do You wish to SORT or SEARCH ---");
+            System.out.println("1. SORT");
+            System.out.println("2. SEARCH");
+            System.out.println("3. ADD RECORDS");
+            System.out.println("4. Create a binary tree");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            
+
+            switch (choice) {
+                case 1:
+                    System.out.println("under construction");
+                    break;
+                case 2:
+                    
+                    System.out.println("under construction");
+                    break;
+                case 3:
+                    System.out.println("under construction");
+                    break;
+                case 4:
+                    System.out.println("under construction");
+                    break;
+                case 5:
+                    // Exit the program
+                    System.out.println("Exiting program.");
+                    
+                    return;
+                default:
+                    // Handles invalid menu choices
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+    
+
+        
     }
     
 }
